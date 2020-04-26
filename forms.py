4fc373b8f15085d0aca86425222f3be711dc55e9
Shivam Panjeta <shivam.panjeta@gmail.com>
2020-04-27 05:06:09 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -5,7 +5,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class LinkForm(FlaskForm):
-    link = StringField('Link', validators=[DataRequired()])
+    link = StringField('Link', [validators.Length(min=3, max=1000)])
 
     sum =  StringField('Sum', widget=TextArea())
 
